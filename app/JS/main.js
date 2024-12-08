aa44db56ef98f2b957e99a24195e257753b6900a
johnnyw <jwu38438@gmail.com>
2024-12-08 13:04:42 -0500

    e

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,4 +1,22 @@
 import '../CSS/style.css';
+const apiKey = import.meta.env.VITE_HYPIXEL_API_KEY;
+const search_form =  document.querySelector(".search_form");
+search_form.addEventListener("submit", (event)=>
+{
+    event.preventDefault()
+    const player_name = document.querySelector(".player_input").value;
+    if(player_name)
+    {
+        retrievePlayerAuctions(player_name)
+    }
+    console.log("Not a valid input")
+})
+async function retrievePlayerAuctions(player_name)
+{
+    const playerUUID =  await getPlayerUUID(player_name);
+    const auction_data =  await getData(`https://api.hypixel.net/v2/skyblock/auction?key=${apiKey}&player=${playerUUID}`)
+    console.log(auction_data)
+}
 
 function abbreviateItem(number) {
     if (number < 1000) return Math.round(number.toString());
@@ -13,13 +31,23 @@ function fetchHead(Base64)
     Base64  = Base64.substring(Base64.lastIndexOf('/')+1);
     return(`https://mc-heads.net/head/${Base64}`)
 }
-function getItemImage()
+async function getPlayerUUID(player_name)
 {
+    try
+    {
+        const uuidResponse =  await getData(`https://api.ashcon.app/mojang/v2/user/${player_name}`);
+        return uuidResponse.uuid.replace(/-/g,"")
 
+    }
+    catch(error)
+    {
+        console.error("Problem fetching player UUID", error);
+    }
 }
+
 //Function for getting item image.
 //Check item within id hash map and cross reference that 
-async function getData(url)
+async function getData(url,error_message)
 {
     try{
         const response = await fetch(url);
@@ -28,32 +56,80 @@ async function getData(url)
     }
     catch(error)
     {
-        console.error("PROBLEM FETCHING DATA", error);
+        console.error(error_message, error);
+        throw error;
     }
 }
 let bazaarData;
-async function load_data()
+
+function refresh_data()
 {
-    bazaarData =  await getData('https://api.hypixel.net/v2/skyblock/auctions');
-    for(const [item_name, details]  of Object.entries(bazaarData.auctions))
+    //When data is refreshed add it to a cache or smth
+    load_data().then(data=>
     {
-        console.log(details);
-    }
-    console.log(Object.keys(bazaarData.auctions).length)
+
+        data.auctions.forEach(auction=>
+        {
+            console.log(auction.start)
+        }
+        )
+        const epochTime =  Date.now()
+        console.log("current time", epochTime)
+    })
+    .catch(error=>{console.error("ERROR REFRESHING DATA", data)})
+
 }
-load_data();
+//Rrefresh data funct
+refresh_data()
+//Create condition to check whether item already exists within list or not. 
+//Impelemnt use of proper caching to avoid strain
+function displayItems()
+{
+    
+}
+//Get images
+//Get the item price - Simple just use the data provided to us
+//This is for displaying the regular stuff 
 
 //Data updates every minute\
-
-async function getPlayerUUID(player_name)
+const resourcehashmap = {};
+async function getItemData()
 {
-    try
-    {
-        const uuidResponse =  await getData(`https://api.ashcon.app/mojang/v2/user/${player_name}`);
-        return uuidResponse;
+    try{
+        const resourcesResponse =  await getData('https://api.hypixel.net/resources/skyblock/items');
+        resourcesResponse.items.forEach(item => {
+            if(!resourcehashmap[item.name])
+            {
+                resourcehashmap[item.name] = [];
+            }
+            resourcehashmap[item.name].push(item)
+        });
+        console.log(resourcehashmap)
+        //Hash map by name instead, more efficent considering how the auction displays data
     }
     catch(error)
     {
-        console.error("Problem fetching player UUID", error);
+        console.error(error,"ERROR");
     }
 }
+async function load_data()
+{
+   return await getData('https://api.hypixel.net/v2/skyblock/auctions', "FAILED TO RETRIEVE AUCTION");
+}
+getItemData()
+function getItemImage(image_name)
+{
+    Promise.all()
+    resourcehashmap[image_name];   
+}
+async function manual_update()
+{
+
+}
+
+//Pull auction data
+//Pull resource data 
+//cache the resource data to avoid making multiple requests
+//Make a cache for player_uuid ig 
+
+//Call method to display item
